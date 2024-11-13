3bc8664454a14e77030b143d5aeda0e30c43e715
sachin <{ID}+{username}@users.noreply.github.com>
2024-11-13 23:42:26 +0100

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  //base: 'https://github.com/slabstech/gaganyatri.in',
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  preview: {
+    port: 11080,
+    strictPort: true,
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    hmr: {
+      overlay: false
+    },
+    port: 11080, // This is the port which we will use in docker
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
+    }
 })
