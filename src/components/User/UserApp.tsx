f995084103d83a2d8c51a158d08afbb7f0ca1dfe
sachin <{ID}+{username}@users.noreply.github.com>
2024-11-20 14:03:12 +0100

    draft- add grid components

--- src/components/User/UserApp.tsx
+++ src/components/User/UserApp.tsx
@@ -1,178 +1,55 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import Box from '@mui/material/Box';
-import {Button, Typography} from '@mui/material';
-import {LocalizationProvider, DatePicker} from '@mui/x-date-pickers';
-import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
-import {DataGrid, GridColDef, GridToolbarContainer, useGridApiContext}
-  from '@mui/x-data-grid';
-import {fetchUserAppData} from '../../redux/reducer/UserDataReducer';
-import {RootState, AppDispatch} from '../../redux/store';
-import dayjs,{ Dayjs } from 'dayjs';
+import { Component } from 'react';
+import { Container, Grid, Typography, Divider } from '@mui/material';
+import UserAppointments from './UserApppointments';
 
-interface Message {
-  id: bigint;
-  appointment_day: string;
-  appointment_time: string;
-  doctor_name: string;
-  status: string;
-  observations: string;
-}
+interface AppState {}
 
-const columns: GridColDef<Message>[] = [
-  {field: 'id', headerName: 'ID', width: 90},
-  {
-    field: 'appointment_day',
-    headerName: 'Day',
-    width: 150,
-    editable: false,
-  },
-  {
-    field: 'appointment_time',
-    headerName: 'Time',
-    width: 150,
-    editable: false,
-  },
-  {
-    field: 'doctor_name',
-    headerName: 'Doctor',
-    width: 150,
-    editable: false,
-  },
-  {
-    field: 'status',
-    width: 150,
-    headerName: 'Status',
-    editable: false,
-  },
-  {
-    field: 'observations',
-    headerName: 'Observations',
-    width: 150,
-    editable: false,
-  },
-];
-
-/** render
- * @return {return}
- */
-function CustomExportButton() {
-  const apiRef = useGridApiContext();
-
-  const handleExport = () => {
-    apiRef.current.exportDataAsCsv();
-  };
-
-  return (
-    <Button onClick={handleExport}>
-      Download CSV
-    </Button>
-  );
-}
-
-/** render
- * @return {return}
- */
-function CustomToolbar() {
-  return (
-    <GridToolbarContainer>
-      <CustomExportButton />
-    </GridToolbarContainer>
-  );
-}
-
-const todayDate = new Date().toISOString().slice(0, 10);
-const today = new Date();
-const nextSevenDays = new Date(today.getTime() +
-  (7 * 24 * 60 * 60 * 1000)).toISOString().slice(0, 10);
-
-const UserApp: React.FC = () => {
-  const [timerows, setTimerows] = useState<Array<Message>>([]);
-  const [loading, setLoading] = useState(true); // add loading state
-
-  const dispatch = useDispatch<AppDispatch>();
-  const [startDate, setStartDate] = useState<Dayjs | null>(null);
-  const [endDate, setEndDate] = useState<Dayjs | null>(null);
-
-  const userId = 1;
-
-  const userDataList = useSelector((state: RootState) =>
-    state.userDataList.userData);
-
-  //console.log(userDataList);
-
-  useEffect(() => {
-    if (loading) {
-    dispatch(
-      fetchUserAppData({
-        page: 1,
-        appointment_day_after: todayDate,
-        appointment_day_before: nextSevenDays,
-        user_id: userId,
-        rejectValue: 'Failed to fetch Appointment.',
-      })
-  ).then(() => setLoading(false));
-}
-}, [dispatch, todayDate, nextSevenDays, userId, loading]);
-
-  useEffect(() => {
-    if (startDate && endDate) {
-      const filteredData = userDataList.filter((item:any) => {
-        const itemDate = dayjs(item.appointment_day);
-        return itemDate.isAfter(startDate) && itemDate.isBefore(endDate);
-      });
-      setTimerows(filteredData);
-    } else {
-      setTimerows(userDataList);
-    }
-  }, [userDataList, startDate, endDate]);
-
-  return (
-    <Box sx={{height: '100%'}}>
-      <Typography variant="h6" gutterBottom>
-        Appointment
-      </Typography>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        label="Start Date"
-        value={startDate}
-        onChange={(newValue) => {
-          setStartDate(newValue);
-        }}
-      />
-      <DatePicker
-        label="End Date"
-        value={endDate}
-        onChange={(newValue) => {
-          setEndDate(newValue);
-        }}
-      />
-    </LocalizationProvider>
-    <DataGrid
-        rows={timerows}
-        columns={columns}
-        slots={{
-          toolbar: CustomToolbar,
-        }}
-        slotProps={{
-          toolbar: {
-            csvOptions: {allColumns: true, fileName: 'gridData'},
-          },
-        }}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 10,
-              page: 0,
-            },
-          },
-        }}
-        pageSizeOptions={[10, 25, 50]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
-  );
+type UserAppProps = {
+  serverUrl: string;
 };
+
+class UserApp extends Component<UserAppProps, AppState> {
+  ollamaBaseUrl = import.meta.env.VITE_OLLAMA_BASE_URL;
+  serverBaseUrl = import.meta.env.VITE_HF_SPACES_URL;
+  isOnline = true;
+
+  constructor(props: UserAppProps) {
+    super(props);
+    this.serverBaseUrl = this.props.serverUrl;
+  }
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Typography variant="h4" gutterBottom>
+            User Information
+          </Typography>
+        </Container>
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <UserAppointments />
+            </Grid>
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+            <Grid item xs={12}>
+              <UserAppointments />
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <UserAppointments />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <UserAppointments />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default UserApp;
