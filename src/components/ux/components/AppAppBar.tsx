e3a886a70fb8bbc76b7a44eaebe00006cf4270bf
sachin <{ID}+{username}@users.noreply.github.com>
2024-11-25 21:16:55 +0100

    fix-build issue 1

--- src/components/ux/components/AppAppBar.tsx
+++ src/components/ux/components/AppAppBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
